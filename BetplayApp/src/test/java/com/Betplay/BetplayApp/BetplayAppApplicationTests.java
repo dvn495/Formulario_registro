ffc94dca1cd7ff848218801c458dade0adfc672a
Nicolas Pedraza Maldonado <152308538+dvn495@users.noreply.github.com>
2024-07-17 09:01:20 -0500

    Add files via upload

---
+++ BetplayApp/src/test/java/com/Betplay/BetplayApp/BetplayAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Betplay.BetplayApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BetplayAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
